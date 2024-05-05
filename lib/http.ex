8cd5cdc3e7e061d7f17a3c6027ab0286b8c75ec5
Deivison-Costaa <139662466+Deivison-Costaa@users.noreply.github.com>
2024-05-05 16:09:03 -0300

    Separação do HTML, melhorias no código e melhorias visuais

--- lib/http.ex
+++ lib/http.ex
@@ -1,47 +1,14 @@
 defmodule Http do
   def pick_image_title() do
-    {:ok,x} = File.read("lib/data/fig4.png")
+    {:ok, x} = File.read("lib/data/fig4.png")
     imagem_base64 = Base.encode64(x)
-    '<td colspan="7" style="text-align: center;"><img src="data:image/jpeg;charset=utf-8;base64,#{imagem_base64}" style = "width: 800px;"></td>'
+    '<td colspan="7" style="text-align: center;"><img src="data:image/jpeg;charset=utf-8;base64,#{imagem_base64}" style="display: block; margin: 0 auto; width: 800px;"></td>'
   end
-  def pick_images_covers(_cont,str,[]), do: str
-  def pick_images_covers(cont,str,list) do
-    cont = cont + 1
-    elemento = hd(list)
-    path = elemento["path"]
-    capa = elemento["url_image"]
-    case cont do
-      1 ->
-        str = str <>"""
-                <tr>
-                        <td>
-                            <a href = "http://localhost:8000/work/#{path}">
-                                <img src="#{capa}" class = "imagem">
-                            </a>
-                        </td>
-           """
-      pick_images_covers(cont,str,tl(list))
-      7 ->
-        str = str <>"""
-                        <td>
-                            <a href = "http://localhost:8000/work/#{path}">
-                                <img src="#{capa}" class = "imagem">
-                            </a>
-                        </td>
-                </tr>
-           """
-      pick_images_covers(cont,str,tl(list))
-      _ ->
-        str = str <>"""
-                        <td>
-                            <a href = "http://localhost:8000/work/#{path}">
-                                <img src="#{capa}" class = "imagem">
-                            </a>
-                        </td>
-           """
-      pick_images_covers(cont,str,tl(list))
-    end
+
+  def pick_images_covers(list) do
+    EEx.eval_file("lib/front_end/capas.html.eex", assigns: [list: list, cont: Enum.count(list)])
   end
+
 
   def gera_resposta (pacote) do
     analisa_req(pacote)
@@ -54,60 +21,13 @@ defmodule Http do
   def resposta({"GET", "/"}) do
     title_image = pick_image_title()
     capas = Information.get_cover_infomation()
-    mangas = pick_images_covers(0,"",capas)
-    index = [
-      """
-          <!DOCTYPE html>
-          <html lang="pt-br">
-          <head>
-              <meta charset="UTF-8">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <title>Manga Reader</title>
-              <style>
-                  /* Estilos opcionais */
-                  table {
-                      border-collapse: collapse;
-                      width: 100%;
+    mangas = pick_images_covers(capas)
 
-                  }
-                  th, td {
-                      /*border: 1px solid black;*/
-                      padding: 8px;
-                      text-align: center;
-                  }
-                  .imagem{
-                      width: 300px;
-                      height: 533px;
-                      max-width: 100%; /* Garante que a imagem não ultrapasse a largura da coluna */
-                      height: auto; /* Mantém a proporção da imagem */
-                      display: block; /* Remove espaços em branco indesejados */
-                      margin-left: 40px;
-                  }
-                  body{
-                      justify-content: center; /* Centraliza no eixo horizontal */
-                      align-items: center;
-                      margin: 0 auto;
-                      display: flex;
-                      height: 80vh;
-                  }
-              </style>
-          </head>
-          <body>
-
-          <table style = "margin-top: 80px;">
-
-              <tr>
-                  #{title_image}
-              </tr>
-                #{mangas}
-          </table>
-
-          </body>
-          </html>
-"""
-    ]
-    resp200(index)
+    EEx.eval_file("lib/front_end/template.html.eex", [title_image: title_image, mangas: mangas])
+    |> String.split("\r\n")
+    |> resp200()
   end
+
 
   # Pagina dos capítulos
   def resposta({"GET", "/chapts/" <> file}) do
--- lib/mangapage.ex
+++ lib/mangapage.ex
@@ -6,69 +6,37 @@ defmodule MangaPage do
     "<td> <center> <img src='data:image/jpeg;charset=utf-8;base64,#{imagem_base64}'></img>"
   end
 
-  def generate_chapt(l2, path, pgs, len) when pgs <= len do
-    pgs_string = Integer.to_string(pgs) |> String.pad_leading(3, "0")
-    path_jpg = path <> "#{pgs_string}.jpg"
-    path_png = path <> "#{pgs_string}.png"
-    path_webp = path <> "#{pgs_string}.webp"
+  def generate_chapt(path, len) do
+    Enum.reduce(1..len, [], fn pgs, acc ->
+      pgs_string = Integer.to_string(pgs) |> String.pad_leading(3, "0")
+      path_jpg = path <> "#{pgs_string}.jpg"
+      path_png = path <> "#{pgs_string}.png"
+      path_webp = path <> "#{pgs_string}.webp"
 
-    image_path =
-      cond do
-        File.exists?(path_jpg) -> path_jpg
-        File.exists?(path_png) -> path_png
-        File.exists?(path_webp) -> path_webp
-        true -> nil
+      image_path =
+        cond do
+          File.exists?(path_jpg) -> path_jpg
+          File.exists?(path_png) -> path_png
+          File.exists?(path_webp) -> path_webp
+          true -> nil
+        end
+
+      case image_path do
+        nil -> acc
+        _ -> acc ++ [reader_file(image_path)]
       end
-
-    case image_path do
-      nil -> generate_chapt(l2, path, pgs+1, len)
-      _ ->
-        l2 = l2 ++ [reader_file(image_path)]
-        generate_chapt(l2, path, pgs+1, len)
-    end
+    end)
   end
-  def generate_chapt(l2, _, pgs, len) when pgs > len, do: l2
+
 
 
 
   def create_manga_page(path, t) do
-    # Retorna a Página Html
-    title = t
     { :ok, len } = Information.count_files_in_directory(path)
-    l2 = []
-    l2 = generate_chapt(l2, path, 1, len)
+    images = generate_chapt(path, len)
 
-    l1 = [
-      '<!DOCTYPE html>',
-      '<html lang="en">',
-      '<head>',
-      '<meta charset="UTF-8">',
-      '<meta name="viewport" content="width=device-width, initial-scale=1.0">',
-      '<style>',
-      'body {',
-      'font-family:Arial;',
-      'color: white;',
-      'background-color:black;',
-      '}',
-      'h1 {',
-      'font-family:Arial;',
-      'font-size: 36pt;',
-      'color: white;',
-      'text-align: center;',
-      '}',
-      '</style>',
-      '<title>#{title}</title>',
-      '</head>',
-      '<body>',
-      '<h1>#{title}</h1>'
-    ]
-
-    l3 = [
-      '</body>',
-      '</html>'
-    ]
-
-    l1 ++ l2 ++ l3
+    EEx.eval_file("lib/front_end/manga_page.html.eex", [title: t, images: images])
+    |> String.split("\r\n")
   end
 
 end
